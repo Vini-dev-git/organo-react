5a83b64efa2fb7aa94ba88dc6ed88e0bbd964a9a
Vini-dev-git <vinaopf@gmail.com>
2023-04-04 22:07:36 -0300

    Start project

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,8 @@
+export const Banner = () => {
+    return (
+        <header>
+            <img src="/imagens/Banner.png" alt="Banner principal da página" />
+        </header>
+        
+    )
+}
---
+++ src/componentes/TextField/index.js
@@ -0,0 +1,12 @@
+import './TextField.css'
+
+const TextField = () => {
+    return (
+        <div className="text-field">
+            <label>Nome:</label>
+            <input placeholder='Digite seu nome'/>
+        </div>
+    )
+}
+
+export default TextField;
